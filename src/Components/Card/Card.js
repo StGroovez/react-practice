f1f795612824ba0752cfd05dd29753536b4393a2
Abdo <abdo180294@gmail.com>
2021-06-23 00:47:23 -0500

    Card and Section Components updated. PropTypes installed.

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import './style.css'
+import Proptypes from 'prop-types'
 
-export const Card = () => {
+export const Card = ({title, imageRandom, text}) => {
+
     return (
         <div className='card text-center'>
-            
+            <img src={imageRandom} alt="" />
             <div className="card-body">
-                <h4 className='card-title'>Card Text</h4>
-                <p className='card-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita ad corporis inventore, tempora assumenda quo suscipit ab tenetur sequi, blanditiis hic nesciunt esse officiis eos praesentium? Quae sequi distinctio minus.</p>
+                <h4 className='card-title'>{title}</h4>
+                <p className='card-text'>
+                    {
+                        text ? text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita ad corporis inventore, tempora assumenda quo suscipit ab tenetur sequi, blanditiis hic nesciunt esse officiis eos praesentium? Quae sequi distinctio minus.'
+                    }
+                </p>
                 <a href="#!" className='btn btn-outline-secondary'>
                     Go to this website
                 </a>
@@ -15,6 +21,13 @@ export const Card = () => {
         </div>
     )
 }
+
+Card.propTypes = {
+    title: Proptypes.string.isRequired,
+    imageRandom: Proptypes.string,
+    text: Proptypes.string
+}
+
 
 
 
--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -4,34 +4,36 @@ import { Card } from '../Card/Card'
 const cards = [
     {
         id: 1,
-        title: 'tarjeta 1',
-        image: "https://source.unsplash.com/random" 
-    },
-    {
-        id: 3,
-        title: 'tarjeta 1',
+        title: 'card 1',
         image: "https://source.unsplash.com/random" 
     },
     {
         id: 2,
-        title: 'tarjeta 1',
+        title: 'card 2',
         image: "https://source.unsplash.com/random" 
+    },
+    {
+        id: 3,
+        title: 'card 3',
+        image: "https://source.unsplash.com/random",
+        text: 'jaja' 
     }
 
 ]
 
 export const Section = () => {
+
+
     return (
         <div className='container d-flex justify-content-center align-items-center h-100'>
             <div className="row">
                 { 
                     cards.map(card => (
-                        <div className="col-md-4" key= {card.id}>
-                            <Card />
+                        <div className="col-md-4" key= {card.id}> 
+                            <Card title={card.title} imageRandom={card.image} text={card.text}/>
                         </div>
                     ) )
                 }    
-
             </div>
         </div>
     )
